3508bba774dd918af72a36d7c087eb23178af721
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-02-28 10:07:44 +0900

    babelfiy

--- dist/egul.js
+++ dist/egul.js
@@ -1,3 +1,6 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
 window.licker = window.licker || {};
 (function (ns, $) {
   'use strict';
@@ -31,7 +34,7 @@ window.licker = window.licker || {};
           $.ajax({
             url: fileName,
             dataType: 'text',
-            success: function (res) {
+            success: function success(res) {
               var srcTxt = res;
 
               var $frame = $codeFrame.clone();
@@ -52,3 +55,5 @@ window.licker = window.licker || {};
     })();
   });
 })(window.licker, window.jQuery);
+
+},{}]},{},[1]);
